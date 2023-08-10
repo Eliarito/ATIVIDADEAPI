d97e6d5e7ffc663d4f7f7e279d4dfe0ce88a4f59
Eliarito <133966592+Eliarito@users.noreply.github.com>
2023-08-10 10:58:59 +0000

    Atualização

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import { selectUsuarios } from "./bd.js";
 import express from "express";      // Requisição do pacote do express
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 import dotenv from "dotenv";        //index.js
+app.use(express.json());
 
 dotenv.config();
 
@@ -11,6 +12,27 @@ app.get("/usuarios", async (req, res) => {
   try {
     const usuarios = await selectUsuarios();
     res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
